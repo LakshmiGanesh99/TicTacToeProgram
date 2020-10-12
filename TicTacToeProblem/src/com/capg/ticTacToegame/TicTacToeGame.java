ae2165a906c2037c94371f849142f7828f94fe09
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-12 23:18:27 +0530

    [LakshmiGanesh] add: player choice option added

--- TicTacToeProblem/src/com/capg/ticTacToegame/TicTacToeGame.java
+++ TicTacToeProblem/src/com/capg/ticTacToegame/TicTacToeGame.java
@@ -4,17 +4,31 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	static Scanner sc = new Scanner(System.in);
-	
+
+	private static final char Character_X = 'X';
+	private static final char Character_Y = 'O';
+
 	public static void main(String[] args) {
 		createBoard();
+		System.out.println("Enter your choice 'X' or 'O'");
+		playerChoice(sc.next().charAt(0));
 	}
-	
+
 	public static char[] createBoard() {
 		char[] ticTacBoard = new char[10];
-		for(int i=1; i<10; i++) {
+		for (int i = 1; i < 10; i++) {
 			ticTacBoard[i] = ' ';
 		}
 		return ticTacBoard;
 	}
-	
+
+	public static char playerChoice(char playerChoice) {
+		char computerChoice;
+		if (playerChoice == Character_X) {
+			computerChoice = Character_Y;
+		} else {
+			computerChoice = Character_Y;
+		}
+		return computerChoice;
+	}
 }
