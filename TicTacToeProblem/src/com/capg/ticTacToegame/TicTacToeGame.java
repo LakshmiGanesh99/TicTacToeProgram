e18e1074a28c68293601d41e365eb066846e85bc
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-12 22:58:22 +0530

    [LakshmiGanesh] add: added the create board method

--- TicTacToeProblem/src/com/capg/ticTacToegame/TicTacToeGame.java
+++ TicTacToeProblem/src/com/capg/ticTacToegame/TicTacToeGame.java
@@ -1,7 +1,20 @@
 package com.capg.ticTacToegame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe Game");
+		createBoard();
 	}
+	
+	public static char[] createBoard() {
+		char[] ticTacBoard = new char[10];
+		for(int i=1; i<10; i++) {
+			ticTacBoard[i] = ' ';
+		}
+		return ticTacBoard;
+	}
+	
 }
