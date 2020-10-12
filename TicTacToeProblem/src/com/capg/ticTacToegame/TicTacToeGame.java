fba3dbefe84a263ef69c353df28a3425323c6c9f
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-12 23:49:49 +0530

    [LakshmiGanesh] : conflict resolved

--- TicTacToeProblem/src/com/capg/ticTacToegame/TicTacToeGame.java
+++ TicTacToeProblem/src/com/capg/ticTacToegame/TicTacToeGame.java
@@ -9,16 +9,22 @@ public class TicTacToeGame {
 	private static final char CHARACTER_X = 'X';
 	private static final char CHARACTER_Y = 'Y';
 
+	// main
 	public static void main(String[] args) {
 
 		createBoard();
+
 		char playerChoice = sc.next().charAt(0);
 		computerChoice(playerChoice);
+
+		int index = sc.nextInt();
+		playerMove(index, playerChoice);
 		printBoard();
 	}
 
 	static char[] ticTacBoard;
 
+	// UC1
 	public static char[] createBoard() {
 		ticTacBoard = new char[10];
 		for (int i = 0; i < 10; i++) {
@@ -27,6 +33,7 @@ public class TicTacToeGame {
 		return ticTacBoard;
 	}
 
+	// UC2
 	public static char computerChoice(char playerChoice) {
 		char computerChoice;
 		if (playerChoice == CHARACTER_X) {
@@ -37,11 +44,38 @@ public class TicTacToeGame {
 		return computerChoice;
 	}
 
+	// UC3
 	public static void printBoard() {
 		System.out.println(" " + ticTacBoard[1] + " | " + ticTacBoard[2] + " | " + ticTacBoard[3]);
 		System.out.println("-----------");
 		System.out.println(" " + ticTacBoard[4] + " | " + ticTacBoard[5] + " | " + ticTacBoard[6]);
 		System.out.println("-----------");
 		System.out.println(" " + ticTacBoard[7] + " | " + ticTacBoard[8] + " | " + ticTacBoard[9]);
+	}
+
+	// UC4
+	public static int selectIndex(int index) {
+		while (!(index > 0 && index < 10)) {
+			System.out.println("Enter proper index value from 1-9");
+			index = sc.nextInt();
+			System.out.println("select" + index);
+		}
+
+		return index;
+	}
+
+	public static int checkForFreeSpace(int index) {
+		index = selectIndex(index);
+		while (ticTacBoard[index] != ' ') {
+			System.out.println("Your selection is not a free space. Enter new index.");
+			index = sc.nextInt();
+		}
+		return index;
+	}
+
+	// UC5
+	public static void playerMove(int index, char choice) {
+		index = checkForFreeSpace(index);
+		ticTacBoard[index] = choice;
 	}
 }
